6e1f47a0de3636c5eaef7fb3b4881b5be284d3a9
Kerbix <kerbixcs@mail.ru>
2024-08-16 19:12:32 +0700

    Added interface of repository and SongRepository

---
+++ ChillMusicUWP/Data/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using ChillMusicUWP.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChillMusicUWP.Data.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+    }
+}
